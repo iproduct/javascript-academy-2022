2ec406ed81c51d94782c7d8154c0c9d891daccfe
Trayan Iliev <office@iproduct.org>
2022-02-27 17:03:28 +0200

    Delete functionality added.

--- js-academy-03-rest/js/main.js
+++ js-academy-03-rest/js/main.js
@@ -2,8 +2,10 @@ import { Post } from "./post-model.js";
 
 window.addEventListener('load', init);
 
-const BASE_API_URL = 'http://localhost:3000/api/';
+const BASE_API_URL = 'http://localhost:3000/api';
 const resultsElement = document.getElementById('results');
+const messagesElement = document.getElementById('messages');
+const errorsElement = document.getElementById('errors');
 
 async function init() {
     const form = document.getElementById('new-post-form');
@@ -21,7 +23,7 @@ function onSubmit(event) {
 }
 
 async function postNewPost(post) {
-    const resp = await fetch(BASE_API_URL + 'posts',{
+    const resp = await fetch(BASE_API_URL + '/posts',{
         headers:{
             'Content-Type': 'application/json'
         },
@@ -31,30 +33,52 @@ async function postNewPost(post) {
     const created = await resp.json();
     const postElem = createArticleElement(created);
     resultsElement.appendChild(postElem);
+    messagesElement.textContent  = '';
+    errorsElement.textContent  = '';
+}
+
+async function deletePost(postId) {
+    const resp = await fetch(`${BASE_API_URL}/posts/${postId}`, {
+        method: 'DELETE',
+    });
+    const deleted = await resp.json();
+    errorsElement.textContent  = '';
+    messagesElement.textContent  = `Successfully deleted post with ID='${postId}'.`;
+    fetchPosts();
 }
 
 async function fetchPosts() {
     resultsElement.innerHTML='';
-    const resp = await fetch(BASE_API_URL + 'posts');
+    const resp = await fetch(BASE_API_URL + '/posts');
     const posts = await resp.json();
     console.log(posts);
     posts.map(post => createArticleElement(post))
         .forEach(postElem => {
             resultsElement.appendChild(postElem);
         });
+    errorsElement.textContent  += '';
 }
 
 
 function createArticleElement(post) {
     const article = document.createElement('article');
+
     const h2 = document.createElement('h2');
     h2.textContent = post.title;
     article.appendChild(h2)
+
     const content = document.createElement('div');
     content.textContent = post.content;
     article.appendChild(content)
+
     const tags = document.createElement('div');
     tags.textContent = post.tags.join(', ');
     article.appendChild(tags)
+
+    const btnDelete = document.createElement('button');
+    btnDelete.textContent = 'Delete';
+    btnDelete.addEventListener('click', () => deletePost(post.id))
+    article.appendChild(btnDelete)
+
     return article;
 }
