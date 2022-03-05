9589011b9637d5833fdda8f244c4c8e632e59a6f
Trayan Iliev <office@iproduct.org>
2022-03-05 17:37:31 +0200

    TodoFilter option added

--- 05-react-todos/src/components/TodoFilter.js
+++ 05-react-todos/src/components/TodoFilter.js
@@ -1,10 +1,13 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ALL_STATUSES } from '../model/todo-model';
 
-const TodoFilter = props => {
+const TodoFilter = ({filter}) => {
   return (
-    <div>TodoFilter</div>
+    <select value={filter}>
+        <option value={ALL_STATUSES}>All</option>
+    </select>
   );
 }
 
