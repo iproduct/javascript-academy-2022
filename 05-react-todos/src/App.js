80f1267ba3926018133ff1ebb522b199ad8fd22c
Trayan Iliev <office@iproduct.org>
2022-03-05 17:34:43 +0200

    TodoFilter component added

--- 05-react-todos/src/App.js
+++ 05-react-todos/src/App.js
@@ -4,9 +4,12 @@ import { useState } from "react";
 import TodoList from './components/TodoList';
 import MOCK_TODOS from "./model/mock-todos";
 import TodoInput from './components/TodoInput';
+import { ALL_STATUSES } from './model/todo-model';
+import TodoFilter from './components/TodoFilter';
 
 function App() {
   const [todos, setTodos] = useState(MOCK_TODOS);
+  const [filter, setFilter] = useState(ALL_STATUSES);
   function createTodo(todo) {
     setTodos([...todos, todo])
   }
@@ -15,12 +18,16 @@ function App() {
   }
   function updateTodo(todo) {
     setTodos(todos.map(td => td.id === todo.id ? todo : td))
+  }  
+  function updateFilter(filter) {
+    setFilter(filter)
   }
   return (
     <div className="App">
       <header className="App-header">
         <h1>React TODOs Demo</h1>
-        <TodoInput onSubmitTodo={createTodo} />
+        <TodoInput onSubmitTodo={createTodo}/>
+        <TodoFilter filter={filter} onFilterChange={updateFilter} />
         <TodoList todos={todos} onDeleteTodo={deleteTodo} onStatusChanged={updateTodo} />
       </header>
     </div>
---
+++ 05-react-todos/src/components/TodoFilter.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TodoFilter = props => {
+  return (
+    <div>TodoFilter</div>
+  );
+}
+
+TodoFilter.propTypes = {};
+
+export default TodoFilter;
