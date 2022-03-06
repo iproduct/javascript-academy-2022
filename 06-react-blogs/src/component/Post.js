279b4a47143043945a0498b557c456de7eec076b
Trayan Iliev <office@iproduct.org>
2022-03-06 17:55:45 +0200

    PostList added to Main comp

--- 06-react-blogs/src/component/Post.js
+++ 06-react-blogs/src/component/Post.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Post = ({blog}) => {
+const Post = ({post}) => {
   return (
-    <div className="card">
+    <div className="card col s12 m4">
     <div className="card-image waves-effect waves-block waves-light">
       <img className="activator" src="images/office.jpg" />
     </div>
     <div className="card-content">
-      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <span className="card-title activator grey-text text-darken-4">{post.title}<i className="material-icons right">more_vert</i></span>
+      <p><a href="#">{post.tags.join(', ')}</a></p>
     </div>
     <div className="card-reveal">
       <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
