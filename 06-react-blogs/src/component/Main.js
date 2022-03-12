aedb28621f920c088830e3b4ee4ee2f5d494e79b
Trayan Iliev <office@iproduct.org>
2022-03-12 18:25:04 +0200

    Main reractored as class component

--- 06-react-blogs/src/component/Main.js
+++ 06-react-blogs/src/component/Main.js
@@ -1,42 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import BlogsClient from '../service/blogs-api-client';
 import PropTypes from 'prop-types';
 import PostList from './PostList';
 import { TabContainer } from './TabContainer';
 import { TabPanel } from './TabPanel';
 
-const Main = props => {
-    const [blogs, setBlogs] = useState([]);
-    const [messages, setMessages] = useState();
-    const [errors, setErrors] = useState();
-    function clearMessagesAndErors() {
-        setMessages(undefined);
-        setErrors(undefined);
+class Main extends Component {
+    state = {
+        blogs: [],
+        messages: undefined,
+        errors: undefined
     }
-    useEffect(() => {
+
+    clearMessagesAndErors = () => {
+        this.setState({messages: undefined, errors: undefined});
+    }
+
+    componentDidMount() {
         BlogsClient.fetchPosts()
             .then(results => {
-                setBlogs(results)
-                clearMessagesAndErors()
+                this.setState({blogs:results})
+                this.clearMessagesAndErors()
             })
             .catch(err => {
-                setErrors(err)
+                this.setState({erors: err});
             });
-    }, [])
-    return (
-        <div className="container">
-            <div className="section">
-                <div className="messages">{messages}</div>
-                <div className="errors">{errors}</div>
-                <div className="row">
-                    <TabContainer>
-                        <TabPanel id="results" title="All Blogs"> <PostList posts={blogs} /></TabPanel>
-                        <TabPanel id="favourites" title="Favourite Blogs">Test 2 content ...</TabPanel>
-                    </TabContainer>
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="section">
+                    <div className="messages">{this.state.messages}</div>
+                    <div className="errors">{this.state.errors}</div>
+                    <div className="row">
+                        <TabContainer>
+                            <TabPanel id="results" title="All Blogs"> <PostList posts={this.state.blogs} /></TabPanel>
+                            <TabPanel id="favourites" title="Favourite Blogs">Test 2 content ...</TabPanel>
+                            <TabPanel id="settings" title="Blog Settings">Blog settings here ...</TabPanel>
+                        </TabContainer>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 Main.propTypes = {}
