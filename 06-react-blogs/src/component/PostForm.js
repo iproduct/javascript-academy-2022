ea5e8e83c4a44a992369b993f651a90165133db0
Trayan Iliev <office@iproduct.org>
2022-03-13 15:55:31 +0200

    bidirectional binding added to form

--- 06-react-blogs/src/component/PostForm.js
+++ 06-react-blogs/src/component/PostForm.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { DEFAULT_AUTOR_ID } from '../model/post-model';
 import './PostForm.css';
 
-export default class PostForm extends Component {
+export default  class PostForm extends Component {
     //   static propTypes = {second: third}
     state = {
         id: '',
@@ -23,61 +23,68 @@ export default class PostForm extends Component {
 
     handleInputChange = (event) => {
         const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.id;
+
+        console.log(`Setting field: ${name} = ${value}`);
+        this.setState({
+            [name] : value
+        });
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <div class="row">
-                    <div class="input-field col s12">
+                <div  className="row">
+                    <div  className="input-field col s12">
                         <input id="id" type="text" disabled value={this.state.id}/>
-                        <label for="id">ID</label>
+                        <label htmlFor="id">ID</label>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="title" type="text" class="validate" value={this.state.title} onChange={this.handleInputChange}/>
-                        <label for="title">Title</label>
-                        <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                <div  className="row">
+                    <div  className="input-field col s12">
+                        <input id="title" type="text"  className="validate" value={this.state.title} onChange={this.handleInputChange}/>
+                        <label htmlFor="title">Title</label>
+                        <span  className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <textarea id="content" class="materialize-textarea" value={this.state.content} onChange={this.handleInputChange}></textarea>
-                        <label for="content">content</label>
+                <div  className="row">
+                    <div  className="input-field col s12">
+                        <textarea id="content"  className="materialize-textarea" value={this.state.content} onChange={this.handleInputChange}></textarea>
+                        <label htmlFor="content">content</label>
                     </div>
-                    <label for="email">Email</label>
+                    <label htmlFor="email">Email</label>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="tags" type="text" class="validate" value={this.state.tags} onChange={this.handleInputChange}/>
-                        <label for="tags">Tags</label>
-                        <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="imageUrl" type="url" class="validate" value={this.state.imageUrl} onChange={this.handleInputChange}/>
-                        <label for="imageUrl">Blog Image URL</label>
-                        <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                <div  className="row">
+                    <div  className="input-field col s12">
+                        <input id="tags" type="text"  className="validate" value={this.state.tags} onChange={this.handleInputChange}/>
+                        <label htmlFor="tags">Tags</label>
+                        <span  className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12">
+                <div  className="row">
+                    <div  className="input-field col s12">
+                        <input id="imageUrl" type="url"  className="validate" value={this.state.imageUrl} onChange={this.handleInputChange}/>
+                        <label htmlFor="imageUrl">Blog Image URL</label>
+                        <span  className="helper-text" data-error="wrong" data-success="right">Helper text</span>
+                    </div>
+                </div>
+                <div  className="row">
+                    <div  className="input-field col s12">
                         <label>
                             <label>
-                                <input type="checkbox" checked={this.state.active} onChange={this.handleInputChange} />
+                                <input id="active" type="checkbox" checked={this.state.active} onChange={this.handleInputChange} />
                                 <span>Is Active</span>
                             </label>
                         </label>
                     </div>
                 </div>
-                <div className='PostForm-button-panel'>
-                    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-                        <i class="material-icons right">send</i>
+                <div  className='PostForm-button-panel'>
+                    <button  className="btn waves-effect waves-light" type="submit" name="action">Submit
+                        <i  className="material-icons right">send</i>
                     </button>
-                    <button class="btn waves-effect waves-light #ff1744 red accent-3" type="submit" name="action">Reset
-                        <i class="material-icons right">cancel</i>
+                    <button  className="btn waves-effect waves-light #ff1744 red accent-3" type="submit" name="action">Reset
+                        <i  className="material-icons right">cancel</i>
                     </button>
                 </div>
             </form>
