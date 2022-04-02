e453e59b92f17b79bdb310bdc5634ae8581a65d2
Trayan Iliev <office@iproduct.org>
2022-04-02 17:50:50 +0300

    http REST simple GET POST blog posts demo

--- 15-nodejs-lab/http1.js
+++ 15-nodejs-lab/http1.js
@@ -4,6 +4,8 @@ const url = require('url')
 const host = '127.0.0.1';
 const port = 3000;
 
+const DB_FILENAME = 'db.json';
+
 const countries = {
   'Bulgaria': 'Sofia',
   'USA': 'Washington, D.C.'
@@ -29,7 +31,7 @@ server.on('error', (e) => {
   }
 });
 
-fs.readFile(`${__dirname}/db.json`).then(data => {
+fs.readFile(`${__dirname}/${DB_FILENAME}`).then(data => {
   const db = JSON.parse(data);
 
   const server = http.createServer((req, res) => {
@@ -37,17 +39,36 @@ fs.readFile(`${__dirname}/db.json`).then(data => {
     // res.setHeader('Content-Type', 'text/plain');
     // res.end(JSON.stringify(countries))
     const pathname = url.parse(req.url, true).pathname
-    if (req.method === 'GET')
-    if (pathname.slice(1) === 'api/posts') {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(db.posts));
-    } else if (pathname.slice(1) === 'api/users') {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(db.users));
-    } else {
+    if (req.method === 'GET') {
+      if (pathname.slice(1) === 'api/posts') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(db.posts));
+      } else if (pathname.slice(1) === 'api/users') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(db.users));
+      } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' })
         res.end("Not found");
       }
+    } else if (req.method === 'POST') {
+      if (pathname.slice(1) === 'api/posts') {
+        const body = [];
+        req.on('data', chunk => {
+          body.push(chunk);
+        }).on('end', async () => {
+          const bodyStr = Buffer.concat(body).toString();
+          const newPost = JSON.parse(bodyStr);
+          const maxId = db.posts.reduce((acc, val) => Math.max(acc, val.id), 0)
+          console.log("Max ID: ", maxId);
+          newPost.id = maxId + 1;
+          console.log("Created NEW Post: ", newPost);
+          db.posts.push(newPost);
+          await fs.writeFile(DB_FILENAME, JSON.stringify(db, null, 4));
+          res.writeHead(200, { 'Content-Type': 'application/json' })
+          res.end(JSON.stringify(newPost));
+        })
+      }
+    }
   });
 
   server.on('error', (e) => {
