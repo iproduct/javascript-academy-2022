1f83b9d6d971d8ad50d4e45a74a5ebc631b153fb
Trayan Iliev <office@iproduct.org>
2022-04-03 18:12:57 +0300

    connect to mongodb

--- 16-express-blogs-api/src/routes/users-router.js
+++ 16-express-blogs-api/src/routes/users-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const inicative = require('indicative');
-const sendErrorResponse = require('./utils').sendErrorResponse;
-const replace_id = require('./utils').replace_id;
+const sendErrorResponse = require('../utils/utils').sendErrorResponse;
+const replace_id = require('../utils/utils').replace_id;
 const ObjectId = require('mongodb').ObjectId;
 
 
@@ -9,5 +9,7 @@ const router = express.Router();
 
 // Users Router
 router.get('/', async (req, res) => {
-    
+
 })
+
+module.exports = router // default export 
--- 16-express-blogs-api/src/server.js
+++ 16-express-blogs-api/src/server.js
@@ -3,14 +3,14 @@ const http = require('http');
 const path = require('path');``
 const { Server } = require("socket.io");
 const cors = require('cors');
-const morgan = require('morgan');
+const logger = require('morgan');
 const MongoClient = require('mongodb').MongoClient;
 const usersRouter = require('./routes/users-router');
-const sendErrorResponse = require('./utils').sendErrorResponse;
+const sendErrorResponse = require('./utils/utils').sendErrorResponse;
 
 
 const mongoUrl = 'mongodb://127.0.0.1:27017';
-const db = 'blogs2022';
+const dbName = 'blogs2022';
 
 const HOST = '127.0.0.1';
 const PORT = 5000;
@@ -19,14 +19,28 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {transports:	['websocket', 'polling']});
 
-app.use(cors)
+const corsOpts = {
+    origin: 'http://localhost:3000'
+}
 
+// apply express middleware
+app.use(cors(corsOpts))
+app.use(logger('dev'));
+app.use(express.json({limit: '10mb'}))
+
+// add feature routers
+app.use('/api/users', usersRouter);
+
+// add static resources
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+// add index.html -> app only
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
+// add socket.io (websocket) server push api
 io.on('connection', function(socket) {
     socket.join('room1');
     console.log("Client connected: " + socket.id);
@@ -35,6 +49,23 @@ io.on('connection', function(socket) {
         io.of("/").to("room1").emit('chat message', msg);
     })
 })
+
+// Connect to MongoDB and start server
+// Create a new MongoClient
+const client = new MongoClient(mongoUrl);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Connected successfully to MongoDB server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
